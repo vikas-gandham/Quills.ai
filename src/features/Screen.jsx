9a877c15d7c6eb3d064261df29e195ee17a73b33
vikas-gandham <gandhamvikas@gmail.com>
2024-02-08 09:12:08 +0530

    Demo to Remo

--- src/features/Screen.jsx
+++ src/features/Screen.jsx
@@ -29,7 +29,7 @@ function Screen() {
             </Button>
             <Button to="/bookdemo" type="bgtrans">
               <div className="flex items-center justify-center gap-1">
-                <RiRecordCircleLine /> Book a Demo
+                <RiRecordCircleLine /> Book a Remo
               </div>
             </Button>
           </div>
