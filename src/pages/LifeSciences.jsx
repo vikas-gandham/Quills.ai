d53dce0c20e6f0cfb2775434a35f64d7a3684b59
vikas-gandham <gandhamvikas@gmail.com>
2024-02-14 18:56:54 +0530

    completed lifeSciences and created termsCondition and privacyPolicy

--- src/pages/LifeSciences.jsx
+++ src/pages/LifeSciences.jsx
@@ -1,4 +1,5 @@
 import DemoButton from "../features/DemoButton";
+import Testimonials from "../features/Testimonials";
 
 function LifeSciences() {
   return (
@@ -233,6 +234,230 @@ function LifeSciences() {
               </div>
             </div>
           </div>
+        </div>
+      </section>
+      <section id="features" className="relative bg-white pb-28 pt-20 sm:py-32">
+        <div className="relative ">
+          <div className=" md:text-center lg:w-full w-[90%] mx-auto">
+            <h2 className="font-display text-3xl tracking-tight text-slate-900  sm:text-4xl md:text-5xl">
+              Use Cases
+            </h2>
+            <p className="mt-6 text-lg tracking-tight text-slate-700 lg:w-[60%] mx-auto ">
+              Efficient Data Gathering: In the business world, there's often a
+              "hazel" or challenge when it comes to collecting vast amounts of
+              data. With the introduction of Quills, this process is
+              streamlined.
+            </p>
+          </div>
+          <div className="  mt-[50px]  ">
+            <div className=" flex flex-col space-y-10 lg:w-full ">
+              <div className="grid lg:grid-cols-2   bg-[#333]">
+                <div className=" p-12 flex items-center justify-center  lg:order-2 text-white ">
+                  <div className=" lg:w-[80%]">
+                    <h1 className=" font-display text-3xl tracking-tight   sm:text-3xl md:text-4xl">
+                      Prompt-Based Data Extraction
+                    </h1>
+                    <p className=" mt-4 text-lg tracking-tight">
+                      Instead of navigating through complex databases or
+                      spreadsheets, users can simply use intuitive prompts like
+                      'give me top 5 customer names with the highest number of
+                      contracts.' This simplifies the data extraction process
+                      and ensures that even those without technical expertise
+                      can fetch the necessary information.
+                    </p>
+                  </div>
+                </div>
+                <div className="  lg:order-1 lg:border-y lg:border-r  p-12 bg-[#fafafa]    overflow-hidden  ">
+                  <img
+                    src="/key-4.png"
+                    className="   shadow-lg w-[90%] mx-auto shadow-gray-300 rounded-2xl opacity-30"
+                  />
+                </div>
+              </div>
+              <div className="grid lg:grid-cols-2  lg:bg-[#fff] bg-[#333] lg:text-black text-white ">
+                <div className=" p-12 flex items-center justify-center  lg:order-1  ">
+                  <div className=" lg:w-[80%]">
+                    <h1 className=" font-display text-3xl tracking-tight   sm:text-3xl md:text-4xl">
+                      Real-time Business Monitoring
+                    </h1>
+                    <p className=" mt-4 text-lg tracking-tight">
+                      As businesses evolve, there's a need for real-time
+                      monitoring of contracts, sales, and customer interactions.
+                      Quills provides an avenue for instant data retrieval,
+                      ensuring that businesses can react promptly to emerging
+                      trends or issues.
+                    </p>
+                  </div>
+                </div>
+                <div className="  lg:order-2 lg:border-y lg:border-l     overflow-hidden  ">
+                  <img src="/key-1.webp" className="   min-w-full " />
+                </div>
+              </div>
+              <div className="grid lg:grid-cols-2   bg-[#333]">
+                <div className=" p-12 flex items-center justify-center  lg:order-2 text-white ">
+                  <div className=" lg:w-[80%]">
+                    <h1 className=" font-display text-3xl tracking-tight   sm:text-3xl md:text-4xl">
+                      Customizable Data Portals
+                    </h1>
+                    <p className=" mt-4 text-lg tracking-tight">
+                      Once the data is fetched, Quills allows users to arrange
+                      and customize the data presentation according to their
+                      preferences. This ensures that reports, presentations, or
+                      dashboards are tailored to the specific needs of the
+                      business or department.
+                    </p>
+                  </div>
+                </div>
+                <div className="  lg:order-1 lg:border-y lg:border-r      overflow-hidden  ">
+                  <img src="/key-2.webp" className="   min-w-full " />
+                </div>
+              </div>
+              <div className="grid lg:grid-cols-2  lg:bg-[#fff] bg-[#333] lg:text-black text-white ">
+                <div className=" p-12 flex items-center justify-center  lg:order-1  ">
+                  <div className=" lg:w-[80%]">
+                    <h1 className=" font-display text-3xl tracking-tight   sm:text-3xl md:text-4xl">
+                      Client Engagement and Self-Service
+                    </h1>
+                    <p className=" mt-4 text-lg tracking-tight">
+                      With the rise of digital platforms, businesses are
+                      constantly seeking ways to engage with their clients more
+                      effectively. Quills can be integrated into web and mobile
+                      channels, allowing clients to fetch specific data or
+                      reports on-demand.
+                    </p>
+                  </div>
+                </div>
+                <div className="  lg:order-2 lg:border-y lg:border-l     overflow-hidden  ">
+                  <img src="/key-3.webp" className="   min-w-full " />
+                </div>
+              </div>
+              <div className="grid lg:grid-cols-2   bg-[#333]">
+                <div className=" p-12 flex items-center justify-center  lg:order-2 text-white ">
+                  <div className=" lg:w-[80%]">
+                    <h1 className=" font-display text-3xl tracking-tight   sm:text-3xl md:text-4xl">
+                      Enhanced Data Security and Integrity
+                    </h1>
+                    <p className=" mt-4 text-lg tracking-tight">
+                      In the age of cyber threats, ensuring the security and
+                      integrity of business data is paramount. Quills not only
+                      provides efficient data retrieval but also incorporates
+                      security measures to prevent unauthorized access or data
+                      breaches.
+                    </p>
+                  </div>
+                </div>
+                <div className="  lg:order-1 lg:border-y lg:border-r      overflow-hidden  ">
+                  <img src="/key-5.webp" className="   min-w-full " />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="features" className="relative bg-white pb-28 pt-20 ">
+        <div className="relative w-[90%] mx-auto ">
+          <div className=" md:text-center lg:w-full w-[90%] mx-auto">
+            <h2 className="font-display text-3xl tracking-tight text-slate-900  sm:text-4xl md:text-5xl">
+              Solutions
+            </h2>
+            <p className="mt-6 text-lg tracking-tight text-slate-700 ">
+              Explore how our AI-powered solutions can benefit the Life Sciences
+              industry.
+            </p>
+          </div>
+          <div className="  mt-[50px]  ">
+            <div className=" grid lg:grid-cols-2 gap-8  lg:w-[90%] mx-auto ">
+              <div className="border bg-gradient-to-br from-[#eaeaea] to-[#f6f6f6] rounded-3xl overflow-hidden shadow-md">
+                <div className=" p-12 flex items-center justify-center">
+                  <div className="lg:w-[80%]">
+                    <h1 className="font-display text-3xl tracking-tight sm:text-3xl md:text-4xl">
+                      Advanced Technology
+                    </h1>
+                    <p className="mt-4 text-lg tracking-tight">
+                      Experience cutting-edge technology that drives our
+                      platform. Our AI-powered solutions are at the forefront of
+                      innovation, offering you the latest advancements in data
+                      analysis.
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <img />
+                </div>
+              </div>
+              <div className="border bg-gradient-to-br from-[#eaeaea] to-[#f6f6f6] rounded-3xl overflow-hidden shadow-md">
+                <div className=" p-12 flex items-center justify-center">
+                  <div className="lg:w-[80%]">
+                    <h1 className="font-display text-3xl tracking-tight sm:text-3xl md:text-4xl">
+                      Seamless Integration
+                    </h1>
+                    <p className="mt-4 text-lg tracking-tight">
+                      Effortlessly integrate our platform with your existing
+                      software and tools. Our solutions are designed to work
+                      smoothly with the systems you already use, reducing
+                      disruptions and maximizing efficiency.
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <img src="/slack.jpg" />
+                </div>
+              </div>
+              <div className="border bg-gradient-to-br from-[#eaeaea] to-[#f6f6f6] rounded-3xl overflow-hidden shadow-md">
+                <div className=" p-12 flex items-center justify-center">
+                  <div className="lg:w-[80%]">
+                    <h1 className="font-display text-3xl tracking-tight sm:text-3xl md:text-4xl">
+                      Customization
+                    </h1>
+                    <p className="mt-4 text-lg tracking-tight">
+                      Tailor our platform to meet your unique project
+                      requirements. We understand that one size doesn't fit all,
+                      so we offer customizable solutions that adapt to your
+                      specific needs.
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <img src="/mes.jpeg" />
+                </div>
+              </div>
+              <div className="border bg-gradient-to-br from-[#eaeaea] to-[#f6f6f6] rounded-3xl overflow-hidden shadow-md">
+                <div className=" p-12 flex items-center justify-center">
+                  <div className="lg:w-[80%]">
+                    <h1 className="font-display text-3xl tracking-tight sm:text-3xl md:text-4xl">
+                      Real-time Insights
+                    </h1>
+                    <p className="mt-4 text-lg tracking-tight">
+                      Gain real-time access to critical research data for
+                      informed decision-making. Our platform empowers you with
+                      instant insights, helping you stay ahead of the curve.
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <video
+                    autoPlay
+                    src="https://static.quills.ai/quills_stats_one.mp4"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section id="testimonials" className="bg-slate-50 py-20 sm:py-32">
+        <div className=" w-[90%] mx-auto">
+          <div className="mx-auto max-w-2xl md:text-center">
+            <h2 className="font-display text-3xl tracking-tight text-slate-900 sm:text-4xl">
+              Loved by businesses worldwide.
+            </h2>
+            <p className="mt-4 text-lg tracking-tight text-slate-700">
+              Our software is so simple that people can’t help but fall in love
+              with it. Simplicity is easy when you just skip tons of
+              mission-critical features.
+            </p>
+          </div>
+          <Testimonials />
         </div>
       </section>
     </div>
--- src/pages/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy.jsx
@@ -1,8 +1,142 @@
 function PrivacyPolicy() {
+  const anchorList = [
+    "Information We Collect",
+    "How We Use and Share Information",
+    "How We Protect Information",
+    "Your Rights Regarding the Use of Your Personal Information",
+    "Links to Other Websites",
+    "Changes to Our Privacy Policy",
+    "Contact Us",
+  ];
   return (
-    <div className=" my-24 text-center">
-      <div>
-        <h1>PrivacyPolicy</h1>
+    <div className=" w-full py-[120px] bg-white dark:text-black ">
+      <div className=" lg:w-[80%] mx-auto px-6 lg:px-0  ">
+        <div className=" space-y-[50px]">
+          <h1 className=" font-semibold text-center md:text-[40px] text-[30px] leading-[30px] text-[#112448] ">
+            Quills PRIVACY POLICY
+          </h1>
+          <div className=" w-full space-y-4">
+            <p className=" text-justify">
+              Quills (the “Company”) is committed to maintaining robust privacy
+              protections for its users. Our Privacy Policy (“Privacy Policy”)
+              is designed to help you understand how we collect, use and
+              safeguard the information you provide to us and to assist you in
+              making informed decisions when using our Service.
+            </p>
+            <p className=" text-justify">
+              For purposes of this Agreement, “Site” refers to the Company’s
+              website, which can be accessed at https://www.quills.ai. The terms
+              “we,” “us,” and “our” refer to the Company. “You” refers to you,
+              as a user of our Site or our Service. By accessing our Site or our
+              Service, you accept our Privacy Policy and Terms of Use (found
+              here:"
+              <a href="#" className="text-blue-500 underline">
+                https://www.quills.ai/terms-conditions
+              </a>
+              "), and you consent to our collection, storage, use and disclosure
+              of your Personal Information as described in this Privacy Policy.
+            </p>
+          </div>
+          <div className=" w-full grid md:grid-cols-6 grid-cols-1 relative">
+            <div className=" md:col-span-2 col-span-1 md:block hidden pl-4 list-disc space-y-4 sticky top-[100px] h-[max-content]">
+              {anchorList.map((item, index) => (
+                <a
+                  key={index}
+                  className=" list-item text-[#112448]  font-medium text-[14px] leading-[20px] cursor-pointer"
+                >
+                  {item}
+                </a>
+              ))}
+            </div>
+            <div id="1" className=" md:col-span-4  col-span-1 space-y-8">
+              <div className=" space-y-6">
+                <h1 className=" font-medium text-[20px] leading-[20px]">
+                  I. INFORMATION WE COLLECT
+                </h1>
+                <p className=" md:text-[16px] text-[14px] font-light text-[#112448] leading-[175%] text-justify">
+                  We collect “Non-Personal Information” and “Personal
+                  Information.” Non-Personal Information includes information
+                  that cannot be used to personally identify you, such as
+                  anonymous usage data, general demographic information we may
+                  collect, referring/exit pages and URLs, platform types,
+                  preferences you submit and preferences that are generated
+                  based on the data you submit and number of clicks. Personal
+                  Information includes your which you submit to us through the
+                  registration process at the Site.
+                </p>
+                <ul className=" pl-5 space-y-4">
+                  <li className=" space-y-2">
+                    <h1 className=" font-medium text-[20px] leading-[20px]">
+                      1. Information collected via Technology
+                    </h1>
+                    <p className=" md:text-[16px] text-[14px] font-light text-[#112448] leading-[175%] text-justify">
+                      To activate the Service you do not need to submit any
+                      Personal Information other than your email address. To use
+                      the Service thereafter, you do not need to submit further
+                      Personal Information. However, in an effort to improve the
+                      quality of the Service, we track information provided to
+                      us by your browser or by our software application when you
+                      view or use the Service, such as the website you came from
+                      (known as the “referring URL”), the type of browser you
+                      use, the device from which you connected to the Service,
+                      the time and date of access, and other information that
+                      does not personally identify you. We track this
+                      information using cookies, or small text files which
+                      include an anonymous unique identifier. Cookies are sent
+                      to a user’s browser from our servers and are stored on the
+                      user’s computer hard drive. Sending a cookie to a user’s
+                      browser enables us to collect Non-Personal information
+                      about that user and keep a record of the user’s
+                      preferences when utilizing our services, both on an
+                      individual and aggregate basis. The Company may use both
+                      persistent and session cookies; persistent cookies remain
+                      on your computer after you close your session and until
+                      you delete them, while session cookies expire when you
+                      close your browser.
+                    </p>
+                  </li>
+                  <li className=" space-y-2">
+                    <h1 className=" font-medium text-[20px] leading-[20px]">
+                      2. Information you provide us by registering for an
+                      account
+                    </h1>
+                    <p className=" md:text-[16px] text-[14px] font-light text-[#112448] leading-[175%] text-justify">
+                      In addition to the information provided automatically by
+                      your browser when you visit the Site, to become a
+                      subscriber to the Service you will need to create a
+                      personal profile. You can create a profile by registering
+                      with the Service and entering your email address, and
+                      creating a user name and a password. By registering, you
+                      are authorizing us to collect, store and use your email
+                      address in accordance with this Privacy Policy.
+                    </p>
+                  </li>
+                  <li className=" space-y-2">
+                    <h1 className=" font-medium text-[20px] leading-[20px]">
+                      3. Children’s Privacy
+                    </h1>
+                    <p className=" md:text-[16px] text-[14px] font-light text-[#112448] leading-[175%] text-justify">
+                      The Site and the Service are not directed to anyone under
+                      the age of 13. The Site does not knowingly collect or
+                      solicit information from anyone under the age of 13, or
+                      allow anyone under the age of 13 to sign up for the
+                      Service. In the event that we learn that we have gathered
+                      personal information from anyone under the age of 13
+                      without the consent of a parent or guardian, we will
+                      delete that information as soon as possible. If you
+                      believe we have collected such information, please contact
+                      us at
+                      <a href="#" className=" underline text-blue-500">
+                        support@www.quills.ai.
+                      </a>
+                    </p>
+                  </li>
+                </ul>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
